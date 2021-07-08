8e6a6d411a6784980fd234ba812d067f30e50d73
MFRoy <you@example.comMRoy@qa.com>
2021-07-08 16:05:10 +0000

    fixed  simple mistakes

--- programs/example.py
+++ programs/example.py
@@ -14,16 +14,20 @@
     # What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
     
 def endsPy(input):
+    # return input[-2:].lower()=='py'
+        
     a = input.lower()
-    b = a[-2:0]
+    b = a[-2:]
     for character in b:
-        if b == 'py':
+        if b==('py'):
             return True
+        else:
+            return False
+        
     # input= input.lower()
     # input=input[-2:0]
     # if input =="py":
     #     return True
 
-input = "apy"
-print(endsPy(input))
+
 
